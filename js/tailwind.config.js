1caf583bc636743c1067df1ffe360bb4d98d676f
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2022-09-17 20:11:02 +0530

    tailwind & header setup

---
+++ js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+   theme: {
+      screens: {
+         sm: "480px",
+         md: "768px",
+         lg: "976px",
+         xl: "1440px",
+      },
+      extend: {
+         colors: {
+            clifford: "#da373d",
+         },
+      },
+   },
+};
